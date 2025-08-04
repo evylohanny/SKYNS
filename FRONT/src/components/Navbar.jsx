94cc65759d0a6d739da2b6ac6ff3a66572add725
BRENDA MAGANELI CASIMIRO <brenda_casimiro@estudante.sesisenai.org.br>
2025-08-04 16:19:59 -0300

    navbar

---
+++ FRONT/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+
+function Navbar() {
+  return (
+    <div class = "bg-white h-30">
+      <div class = "flex justify-center items-center h-15"  >
+        <input type="text" placeholder="Buscar" class = "border border-gray/50 border-1 rounded-xl p-1 px-5 w-75"/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
