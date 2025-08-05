a254566e4e581c961e98006db15aad30dcd599e2
BRENDA MAGANELI CASIMIRO <brenda_casimiro@estudante.sesisenai.org.br>
2025-08-05 15:51:17 -0300

    footers finalizados

--- FRONT/src/pages/Home.jsx
+++ FRONT/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from '../components/Header';
+import Footer from '../components/FooterTecnico'
+import FooterCompleto from '../components/FooterCompleto';
 
 function Home() {
     
   return (
     <div>
-        <Header />    
+      <FooterCompleto/>
     </div>
   );
 };
