68190f402daf8f1f57e4635aca6684335537a23b
MateusdasilvaDES <mateus_silva44@estudantes.sesisenai.org.br>
2025-08-05 01:50:26 -0300

    mudanças login

--- FRONT/src/pages/Login.jsx
+++ FRONT/src/pages/Login.jsx
@@ -2,6 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/autoplay";
+import { useState } from "react";
 
 function Login() {
   const imagens = [
@@ -26,9 +27,9 @@ function Login() {
     "img_login_cinco.svg",
     "img_login_seis.svg",
     "img_login_sete.svg",
-    
   ];
 
+  const [abaAtiva, setAbaAtiva] = useState("cadastro");
   return (
     <div className="h-screen">
       <div className="flex w-full h-full">
@@ -36,8 +37,7 @@ function Login() {
           <Swiper
             modules={[Autoplay]}
             autoplay={{ delay: 7000, disableOnInteraction: false }}
-            
-            rewind={true} 
+            rewind={true}
             slidesPerView={1}
             speed={800}
           >
@@ -52,8 +52,84 @@ function Login() {
             ))}
           </Swiper>
         </div>
-        <div className="w-1/2 flex items-center justify-center">
-         em andamento.............................
+        <div className="w-1/2 h-full flex flex-col items-center justify-center">
+          <div className="flex w-full  items-center justify-center">
+            <div className="flex w-4/6 space-x-8 border-b border-gray ">
+              <button
+                onClick={() => {
+                  setAbaAtiva("cadastro");
+                }}
+                className={`pb-2 transition-all ${
+                  abaAtiva === "cadastro"
+                    ? "text-purpledark border-b-3 border-purpledark font-bold"
+                    : "text-gray border-b-3 border-transparent"
+                }`}
+              >
+                Cadastro
+              </button>
+
+              <button
+                onClick={() => {
+                  setAbaAtiva("login");
+                }}
+                className={`pb-2 transition-all ${
+                  abaAtiva === "login"
+                    ? "text-purpledark border-b-3 border-purpledark font-bold"
+                    : "text-gray border-b-3 border-transparent"
+                }`}
+              >
+                Login
+              </button>
+            </div>
+          </div>
+          {abaAtiva === "login" && (
+            <div className="flex flex-col w-full  pt-12 ">
+              <div className="flex flex-col w-full justify-center items-center ">
+                <p className="w-4/6 text-5xl ">Que bom ter você aqui!</p>
+                <p className="w-4/6 text-2xl pt-2 ">
+                  Insira suas credenciais para acessar sua conta
+                </p>
+              </div>
+              <div className="flex flex-col w-full justify-center items-center pt-12">
+                <label className="w-4/6 text-2xl" htmlFor="">
+                  Endereço de e-mail
+                </label>
+                <div className="flex w-full pt-5 justify-center items-center ">
+                  <input
+                    className="w-4/6 border-2 border-gray p-2 rounded-lg focus:border-purpledark outline-none "
+                    type="text"
+                    placeholder="ex: ronaldo@gmail.com"
+                  />
+                </div>
+                <label className="w-4/6 text-2xl pt-5" htmlFor="">
+                  Senha
+                </label>
+                <div className="flex w-full pt-5 justify-center items-center ">
+                  <input
+                    className="w-4/6 border-2 border-gray p-2 rounded-lg focus:border-purpledark outline-none "
+                    type="text"
+                    placeholder="ex: ronaldo@gmail.com"
+                  />
+                </div>
+              </div>
+              <div className="flex w-57/100 space-x-2 pt-10 justify-center items-center">
+                <input type="checkbox" />
+                <p>Relembrar por 30 dias</p>
+              </div>
+              <div className="flex  w-full justify-center items-center ">
+                <div className="flex text-white w-full justify-center items-center pt-3">
+                  <button className=" bg-purpledark w-4/6 font-bold  rounded-2xl p-2.5">Login</button>
+                </div>
+              </div>
+              <div className="w-full flex items-center justify-center pt-5 ">
+                <div onClick={""} className="w-4/6 flex justify-center items-center border-2 border-purpledark rounded-2xl p-1 space-x-4 text-purpledark">
+                  <img src="logo_gogle.svg" alt="" />
+                  <p className="font-bold">Entrar com o Google</p>
+                </div>
+              </div>
+            </div>
+          )}
+          {abaAtiva === "cadastro" && <div>cadastro</div>}
         </div>
       </div>
     </div>
