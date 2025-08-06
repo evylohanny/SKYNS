b56fdcd352b44da1a5d14cd375a248ac4b52b449
VitorMatt <vitor.azevedoo2812@gmail.com>
2025-08-06 17:25:35 -0300

    Home

--- FRONT/src/pages/Home.jsx
+++ FRONT/src/pages/Home.jsx
@@ -1,11 +1,28 @@
-import Header from '../components/Header';
-import Footer from '../components/Footer'
+import NavBar from "../components/NavBar";
+import poster from "../assets/poster.svg";
 
 function Home() {
     
   return (
     <div>
-      <Header/>
+      <div className="flex flex-col w-full">
+        <NavBar />
+        <div className="h-screen w-full flex">
+          <div className="h-full w-full relative">
+            <img className="w-full" src={poster} alt="" />
+          </div>
+          <div className="absolute">
+            <p>CUIDAR DA PELE TAMBÉM É CUIDAR DE VOCÊ</p>
+            <h1>Transforme sua rotina em um ritual de acolhimento.</h1>
+            <p>
+              SUA PELE É ÚNICA. DESCUBRA OS INGREDIENTES CERTOS PARA CUIDAR DELA COM CIÊNCIA E CARINHO.
+            </p>
+            <div>
+              <p>Vamos lá</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
