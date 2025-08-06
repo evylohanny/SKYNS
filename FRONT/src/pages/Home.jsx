6b36474016a10eae82b252739bedf28e35c124d7
evylohanny <evelyn_l_silva@estudante.sesisenai.org.br>
2025-08-06 17:13:00 -0300

    navbar finalizada

--- FRONT/src/pages/Home.jsx
+++ FRONT/src/pages/Home.jsx
@@ -1,11 +1,12 @@
 import Header from '../components/Header';
-import Footer from '../components/Footer'
+import NavBar from '../components/NavBar';
 
 function Home() {
     
   return (
     <div>
-      <Header/>
+      {/* <Header/> */}
+      <NavBar />
     </div>
   );
 };
