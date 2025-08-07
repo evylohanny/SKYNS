08318cd9b36e2e6dde2bd16bd629d61a269577d5
VitorMatt <vitor.azevedoo2812@gmail.com>
2025-08-07 17:07:59 -0300

    Home

--- FRONT/src/pages/Home.jsx
+++ FRONT/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import NavBar from "../components/NavBar";
 import poster from "../assets/poster.svg";
+import banner from "../assets/Frame 23864.svg";
 
 function Home() {
     
@@ -7,19 +8,28 @@ function Home() {
     <div>
       <div className="flex flex-col w-full">
         <NavBar />
-        <div className="h-screen w-full flex">
-          <div className="h-full w-full relative">
+        <div className="h-[90vh] w-full flex">
+          <div className="h-[70vh] w-full relative">
             <img className="w-full" src={poster} alt="" />
           </div>
-          <div className="flex flex-col absolute m-30">
-            <p className="text-white font-secondary font-semibold">CUIDAR DA PELE TAMBÉM É CUIDAR DE VOCÊ</p>
-            <h1 className="text-extradarkpurple text-5xl w-150 font-secondary font-bold">Transforme sua rotina em um ritual de acolhimento.</h1>
-            <p>
+          <div className="flex flex-col absolute ml-40 mt-40 gap-2">
+            <p className="text-white text-[17px] leading-[30px] tracking-[2px] font-secondary font-bold">CUIDAR DA PELE TAMBÉM É CUIDAR DE VOCÊ</p>
+            <div className="text-extradarkpurple w-200 font-secondary font-semibold text-[60px] leading-[80px] tracking-[-0.5px] mt-1">Transforme sua rotina em um ritual de acolhimento.</div>
+            <p className="text-white w-190 text-[17px] leading-[30px] tracking-[1.6px] font-secondary font-bold">
               SUA PELE É ÚNICA. DESCUBRA OS INGREDIENTES CERTOS PARA CUIDAR DELA COM CIÊNCIA E CARINHO.
             </p>
-            <div>
-              <p>Vamos lá</p>
+            <div className="group bg-purpledark text-center w-55 p-3 center rounded-full mt-5 hover:bg-white transition duration-300">
+              <p className="text-white text-[23px] font-bold font-secondary hover:text-purpledark transition duration-300 cursor-pointer">Vamos lá</p>
             </div>
+          </div>
+        </div>
+        <div className="h-[100vh] w-full flex flex-col">
+          <div className="flex h-[20vh] w-full items-start justify-center">
+            <img className="w-350 h-fit shadow-lg" src={banner} alt="" />
+          </div>
+          <div className="flex flex-col w-full h-[80vh]">
+            <p className="flex w-full h-fit text-[30px] mt-20 font-secondary tracking-[2px] text-salmon items-center justify-center">Queridinhos da galera</p>
+            <p></p>
           </div>
         </div>
       </div>
