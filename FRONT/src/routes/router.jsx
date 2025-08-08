735d3e40ec2fe5f03109dc862c0f665d4ddc6f39
VitorMatt <vitor.azevedoo2812@gmail.com>
2025-08-07 22:17:15 -0300

    Home

--- FRONT/src/routes/router.jsx
+++ FRONT/src/routes/router.jsx
@@ -1,12 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
-<<<<<<< HEAD
 import Cadastro_login from "../pages/Cadastro_login";
 import Perfil from "../pages/Perfil";
-=======
-import Login from "../pages/Login";
 import Produtos from "../pages/Produtos";
->>>>>>> 99a2fdefd8d0978434129313e2dad1415d1f1267
 
 const router = createBrowserRouter([
     {
@@ -14,7 +10,6 @@ const router = createBrowserRouter([
         element: <Home />
     },
     {
-<<<<<<< HEAD
         path: '/cadastro',
         element: <Cadastro_login/>
     },
@@ -22,16 +17,10 @@ const router = createBrowserRouter([
         path: '/perfil',
         element: <Perfil/>
     },
-    
-=======
-        path: '/login',
-        element: <Login/>
-    },
     {
         path: '/produtos',
         element: <Produtos />
     }
->>>>>>> 99a2fdefd8d0978434129313e2dad1415d1f1267
 ]);
 
 export default router;
