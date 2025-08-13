3116af37c567332f1aea6d63a0ad32403fab24c0
MateusdasilvaDES <mateus_silva44@estudantes.sesisenai.org.br>
2025-08-13 17:21:54 -0300

    mudanças perfil

---
+++ FRONT/src/components/perfil/Dados_pessoais.jsx
@@ -0,0 +1,133 @@
+import { useState } from "react";
+
+function Dados_pessoais() {
+  7;
+
+  const [valor_cpf_editar, setValor_cpf_editar] = useState();
+  const [valor_data_editar, setValor_data_editar] = useState();
+  const [valor_tele_editar, setValor_tele_editar] = useState();
+  const [genero, setGenero] = useState("");
+  const [valor_nome_editar, setvalor_nome_editar] = useState();
+
+  const formatarNome = (value) => value.replace(/[^A-Za-zÀ-ÖØ-öø-ÿ\s]/g, "");
+
+  const formatarCPF = (value) => {
+    value = value.replace(/\D/g, "");
+    value = value.replace(/^(\d{3})(\d)/, "$1.$2");
+    value = value.replace(/^(\d{3})\.(\d{3})(\d)/, "$1.$2.$3");
+    value = value.replace(/^(\d{3})\.(\d{3})\.(\d{3})(\d)/, "$1.$2.$3-$4");
+    return value.slice(0, 14);
+  };
+
+  const formatarData = (value) => {
+    value = value.replace(/\D/g, "");
+    value = value.replace(/(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
+    return value.slice(0, 10);
+  };
+
+  const formatarTele = (value) => {
+    value = value.replace(/\D/g, "");
+    value = value.replace(/^(\d{2})(\d)/, "($1) $2");
+    value = value.replace(/(\d{5})(\d)/, "$1-$2");
+    return value.slice(0, 15);
+  };
+
+  
+  return (
+    <div className="bg-[#F4F4F4] w-60/100 flex flex-col items-center  rounded-2xl">
+      <div className="w-84/100 h-10/100 flex  mt-8 items-center">
+        <h1 className="text-2xl font-medium ">Dados pessoais</h1>
+      </div>
+      <div className="flex flex-col mt-4 w-84/100">
+        <label htmlFor="" className="text-lg">
+          Nome
+        </label>
+        <div className="w-full pt-2">
+          <input
+            type="text"
+            placeholder="Ex: Manassés"
+            className="w-full border-[#D9D9D9] border-2 rounded-lg p-1.5
+             focus:border-purpleborde outline-none "
+             value={valor_nome_editar}
+            onChange={(e) => setvalor_nome_editar(formatarNome(e.target.value))}
+          />
+        </div>
+        <label htmlFor="" className="text-lg pt-5">
+          Email
+        </label>
+        <div className="w-full pt-2">
+          <input
+            type="text"
+            placeholder="Ex: Manassés@gmail.com"
+            className="w-full border-[#D9D9D9] border-2 rounded-lg p-1.5
+             focus:border-purpleborde outline-none "
+          />
+        </div>
+        <label htmlFor="" className="text-lg pt-5">
+          CPF
+        </label>
+        <div className="w-full pt-2">
+          <input
+            type="text"
+            placeholder="Ex: 123.456.789.10"
+            className="w-full border-[#D9D9D9] border-2 rounded-lg p-1.5
+             focus:border-purpleborde outline-none "
+            value={valor_cpf_editar}
+            onChange={(e) => setValor_cpf_editar(formatarCPF(e.target.value))}
+          />
+        </div>
+        <label htmlFor="" className="text-lg pt-5">
+          Data de nascimento
+        </label>
+        <div className="w-full pt-2">
+          <input
+            type="text"
+            placeholder="Ex: 11/11/2000"
+            className="w-full border-[#D9D9D9] border-2 rounded-lg p-1.5
+             focus:border-purpleborde outline-none "
+            value={valor_data_editar}
+            onChange={(e) => setValor_data_editar(formatarData(e.target.value))}
+          />
+        </div>
+        <label htmlFor="" className="text-lg pt-5">
+          Gênero
+        </label>
+        <div className="w-full pt-2">
+          <select
+            value={genero}
+            onChange={(e) => setGenero(e.target.value)}
+            className="w-full flex border-2 border-[#D9D9D9] rounded-lg p-1.5 focus:outline-none  focus:border-purpledark"
+          >
+            <option value="">Selecione</option>
+            <option value="masculino">Masculino</option>
+            <option value="feminino">Feminino</option>
+            <option value="prefiro nao informar">Prefiro não informar</option>
+          </select>
+        </div>
+        <label htmlFor="" className="text-lg pt-5">
+          Telefone
+        </label>
+        <div className="w-full pt-2">
+          <input
+            type="text"
+            placeholder="Ex: (48) 99999-9999"
+            className="w-full border-[#D9D9D9] border-2 rounded-lg p-1.5
+             focus:border-purpleborde outline-none "
+            value={valor_tele_editar}
+            onChange={(e) => setValor_tele_editar(formatarTele(e.target.value))}
+          />
+        </div>
+        <div className="flex w-full justify-end items-center h-40">
+          <button
+            className="p-2 border-purpledark border-2 w-38/100 rounded-lg font-medium text-purpledark 
+            hover:cursor-pointer hover:bg-purpledark hover:text-white hover:transition duration-400 ease-in-out"
+          >
+            EDITAR
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dados_pessoais;
--- FRONT/src/pages/Perfil.jsx
+++ FRONT/src/pages/Perfil.jsx
@@ -1,6 +1,27 @@
 import NavBar from "../components/NavBar";
+import FooterTecnico from "../components/FooterTecnico";
+
+import Dados_pessoais from "../components/perfil/Dados_pessoais";
+import Enderecos from "../components/perfil/Enderecos";
+import Cartoes from "../components/perfil/Cartoes";
+
+import { useState } from "react";
 
 function Perfil() {
+  const [infoAtiva, setInfoAtiva] = useState("dados");
+
+  const ativa_aba_dados = () => {
+    setInfoAtiva("dados");
+  };
+
+  const ativa_aba_end = () => {
+    setInfoAtiva("endereco");
+  };
+
+  const ativa_aba_card = () => {
+    setInfoAtiva("cartoes");
+  };
+
   return (
     <div className="w-full h-full">
       <NavBar />
@@ -21,7 +42,7 @@ function Perfil() {
             Que tal conferir nossas fómulas incríveis?
           </p>
           <div className="pt-6 w-full flex items-center justify-center">
-            <button className="bg-purpledark w-18/100 text-white p-3 text-lg rounded-3xl font-medium">
+            <button className="bg-purpledark w-16/100 text-white p-2 text-md rounded-3xl font-medium">
               Ver Produtos
             </button>
           </div>
@@ -36,29 +57,72 @@ function Perfil() {
         <div className="flex w-76/100 gap-20 ">
           <div className=" bg-[#F4F4F4] w-36/100 h-65/100 rounded-2xl">
             <div className="flex w-full h-32/100 items-center justify-center gap-5">
-                <img src="img_perfil.svg" alt="" />
-                <p className="text-2xl h-10">Manassés Marcelino</p>
+              <img src="img_perfil.svg" alt="" />
+              <p className="text-2xl h-10">Manassés Marcelino</p>
             </div>
             <div className=" pl-10 w-full flex flex-col justify-center items-center">
-                <div className="w-45/100">
-                  <button className="text-2xl cursor-pointer">Dados pessoais</button>
-                </div> 
-                <div className="w-45/100">
-                  <button className="text-2xl pt-6 cursor-pointer">Endereços</button>
-                </div> 
-                <div className="w-45/100">
-                  <button className="text-2xl pt-6 cursor-pointer">Cartoes</button>
-                </div> 
-                <div className="w-45/100">
-                  <button className="text-2xl pt-6 cursor-pointer">Sair</button>
-                </div> 
-                <div className="w-45/100">
-                  <button className="text-2xl pt-6 cursor-pointer">Excluir</button>
-                </div> 
+              <div className="w-50/100">
+                <button
+                  onClick={ativa_aba_dados}
+                  className={`
+                      transition-all cursor-pointer 
+                  ${
+                    infoAtiva === "dados"
+                      ? "text-purpledark text-2xl border-l-3 pl-2  border-purpledark font-medium"
+                      : "text-black  border-l-3 text-lg border-[#FEF5FF] pl-2 font-medium"
+                  }
+                 `}
+                >
+                  Dados pessoais
+                </button>
+              </div>
+              <div className="w-50/100">
+                <button
+                  onClick={ativa_aba_end}
+                  className={`
+                     mt-6 transition-all cursor-pointer 
+                  ${
+                    infoAtiva === "endereco"
+                      ? "text-purpledark text-2xl border-l-3 pl-2  border-purpledark font-medium"
+                      : "text-black text-lg  border-l-3 border-[#FEF5FF] pl-2 font-medium"
+                  }
+                 `}
+                >
+                  Endereços
+                </button>
+              </div>
+              <div className="w-50/100">
+                <button
+                  onClick={ativa_aba_card}
+                  className={`
+                      mt-6 transition-all cursor-pointer 
+                  ${
+                    infoAtiva === "cartoes"
+                      ? "text-purpledark text-2xl border-l-3 pl-2  border-purpledark font-medium"
+                      : "text-black text-lg  border-l-3 border-[#FEF5FF] pl-2 font-medium"
+                  }
+                 `}
+                >
+                  Cartoes
+                </button>
+              </div>
+              <div className="w-45/100">
+                <button className="text-2xl pt-6 cursor-pointer">Sair</button>
+              </div>
+              <div className="w-45/100">
+                <button className="text-2xl pt-6 cursor-pointer">
+                  Excluir
+                </button>
+              </div>
             </div>
           </div>
-          <div className="bg-[#F4F4F4] w-60/100 h-full rounded-2xl">ola</div>
+          {infoAtiva == "dados" && <Dados_pessoais />}
+          {infoAtiva == "endereco" && <Enderecos />}
+          {infoAtiva == "cartoes" && <Cartoes />}
         </div>
+      </div>
+      <div className="pt-30">
+        <FooterTecnico />
       </div>
     </div>
   );
