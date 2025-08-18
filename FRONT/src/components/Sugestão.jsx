1198c4b3cd3c4beb6a2eb8fd9f6bd25477aac323
evylohanny <evelyn_l_silva@estudante.sesisenai.org.br>
2025-08-18 17:21:46 -0300

    Sugestão

--- FRONT/src/components/Sugestão.jsx
+++ FRONT/src/components/Sugestão.jsx
@@ -3,6 +3,8 @@ import { Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import foto1Su from '../assets/foto1Su.svg';
+import setaEsquerda from '../assets/setaEsquerda.svg';
+import setaDireita from '../assets/setaDireita.svg';
 
 function Sugestão() {
   const produtos = [
@@ -34,37 +36,55 @@ function Sugestão() {
   ];
 
   return (
-    <div className="bg-graymedium p-6 rounded-lg">
-      <h2 className="text-lg font-semibold">Aproveite as promoções e complete sua rotina</h2>
-      <p className="text-gray-500 text-sm mb-6">Até 10% OFF na compra de 3 unidades</p>
+    <div className="relative bg-graymedium p-6 rounded-lg">
+      <div className='flex flex-col px-20'>
+        <h2 className="text-lg font-semibold text-gray2">Aproveite as promoções e complete sua rotina</h2>
+        <p className="text-gray2 text-sm mb-6">Até 10% OFF na compra de 3 unidades</p>
+      </div>
 
       <Swiper
         modules={[Navigation]}
-        navigation
+        navigation={{
+          prevEl: ".custom-prev",
+          nextEl: ".custom-next",
+        }}
         spaceBetween={20}
-        slidesPerView={4}
+        slidesPerView={1}
         className="pb-6"
       >
         {produtos.map((item, index) => (
           <SwiperSlide
             key={index}
-            className="bg-white p-4 rounded-lg shadow-md text-center flex flex-col justify-between h-[350px]"
+            className="!flex flex-row px-20 text-center justify-center items-center h-[350px]"
           >
-            <img
-              src={item.img}
-              alt={item.nome}
-              className="object-cover"
-            />
-            <div>
-              <p className="text-sm font-medium mt-3 line-clamp-2">{item.nome}</p>
-              <p className="text-purple-500 font-semibold">{item.preco}</p>
+            {/* repetições de produto */}
+            <div className='flex flex-col w-[20%] justify-center items-start'>
+              <img
+                src={item.img}
+                alt={item.nome}
+                className="object-cover w-[80%]"
+              />
+              <div className='!flex flex-col w-[70%] items-start gap-3'>
+                <p className="text-sm font-medium mt-3 line-clamp-2 text-black/70">{item.nome}</p>
+                <p className="text-purpledark/75 font-semibold">{item.preco}</p>
+              </div>
+              <button className="bg-purpledark text-white px-10 py-2 mt-3 rounded-full font-semibold">
+                LEVAR
+              </button>
             </div>
-            <button className="bg-purple-500 hover:bg-purple-600 text-purpledark px-5 py-2 mt-3 rounded-full font-semibold transition-colors">
-              LEVAR
-            </button>
           </SwiperSlide>
         ))}
       </Swiper>
+
+      {/* Botão Esquerda */}
+      <button className="custom-prev absolute left-4 top-1/2 -translate-y-1/2 z-10">
+        <img src={setaEsquerda} alt="Anterior" className="w-8 h-8" />
+      </button>
+
+      {/* Botão Direita */}
+      <button className="custom-next absolute right-4 top-1/2 -translate-y-1/2 z-10">
+        <img src={setaDireita} alt="Próximo" className="w-8 h-8" />
+      </button>
     </div>
   );
 }
