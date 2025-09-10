48a46e738a8d2be249da81f25ba8196cd16fdd6d
VitorMatt <vitor.azevedoo2812@gmail.com>
2025-09-10 17:21:37 -0300

    [RASTREIO]

--- FRONT/src/pages/Rastreio.jsx
+++ FRONT/src/pages/Rastreio.jsx
@@ -20,54 +20,70 @@ function Rastreio() {
     const changePhase = (phase) => setCurrentPhase(phase);
 
   return (
-    <div className='h-full w-full flex flex-col'>
+    <div className='h-full w-full flex flex-col items-center'>
         <div className='w-full h-fit flex flex-col'>
             <CarrosselPQ />
             <div className='w-full h-22 flex items-center justify-center shadow-xl'>
                 <img src={logo} alt="" />
             </div>
         </div>
-        <div className='w-fit ml-80 mr-80 mt-30 h-full flex flex-col'>
-            <div className='w-full h-10 flex items-center justify-center'>
-                <div className='font-secondary font-medium text-gray2 text-3xl text-start w-125'>Produção</div>
-                <div className='font-semibold flex gap-1 text-purpledark text-[1.3rem] items-center justify-end text-end w-130'><img src={pendentIcon} alt='Ícone de pendente' />Pendente</div>
+        <div className='w-295 mt-20 h-full flex flex-col'>
+            <div className='w-full h-10 flex items-center justify-center gap-[57%]'>
+                <div className='font-secondary font-medium text-gray2 text-3xl opacity-66 text-start'>Andamento</div>
+                <div className='font-semibold flex gap-1 text-purpledark text-[1.3rem] text-end'><img src={pendentIcon} alt='Ícone de pendente' />Pendente</div>
             </div>
             <div>
-                <div className='flex gap-[10%] items-center justify-center mt-15'>
-                    <div className='flex flex-col items-center justify-center'>
-                        <img onClick={() => changePhase('Estoque')} src={concluido} />
+                <div className='flex gap-[7%] w-full h-30 items-center justify-center mt-25'>
+                    <div className='w-30 h-30 flex flex-col justify-end items-center'>
+                        <img className='w-[125px] h-[125px]' onClick={() => changePhase('Estoque')} src={concluido} />
                         <p className='text-center font-semibold text-[1rem] text-purpledark'>Estoque</p>
                     </div>
-                    <div>
-                        <img onClick={() => changePhase('Processo')} src={currentPhase === 'Processo' ? processoConcluido : currentPhase === 'Montagem' || currentPhase === 'Expedição' || currentPhase === 'Completed' ? concluido : processo} />
-                        <p className={clsx('text-center font-semibold text-[1rem]', currentPhase !== 'Estoque' ? 'text-purpledark' : 'text-gray2')}>Processo</p>
+                    <div className='w-30 h-30 flex flex-col justify-end items-center'>
+                        <img className='w-[125px] h-[125px]' onClick={() => changePhase('Processo')} src={currentPhase === 'Processo' ? processoConcluido : currentPhase === 'Montagem' || currentPhase === 'Expedição' || currentPhase === 'Completed' ? concluido : processo} />
+                        <p className={clsx('text-center font-semibold text-[1rem]', currentPhase !== 'Estoque' ? 'text-purpledark' : 'text-gray2 opacity-66')}>Processo</p>
                     </div>
-                    <div>
-                        <img onClick={() => changePhase('Montagem')} src={currentPhase === 'Montagem' ? montagemConcluido : currentPhase === 'Expedição' || currentPhase === 'Completed' ? concluido : montagem} />
-                        <p className={clsx('text-center font-semibold text-[1rem]', currentPhase !== 'Estoque' && currentPhase !== 'Processo' ? 'text-purpledark' : 'text-gray2')}>Montagem</p>
+                    <div className='w-30 h-30 flex flex-col justify-end items-center'>
+                        <img className='w-[125px] h-[125px]' onClick={() => changePhase('Montagem')} src={currentPhase === 'Montagem' ? montagemConcluido : currentPhase === 'Expedição' || currentPhase === 'Completed' ? concluido : montagem} />
+                        <p className={clsx('text-center font-semibold text-[1rem]', currentPhase !== 'Estoque' && currentPhase !== 'Processo' ? 'text-purpledark' : 'text-gray2 opacity-66')}>Montagem</p>
                     </div>
-                    <div>
-                        <img onClick={() => changePhase('Expedição')} src={currentPhase === 'Expedição' ? expedicaoConcluido : currentPhase === 'Completed' ? concluido : expedicao} />
-                        <p className={clsx('text-center font-semibold text-[1rem]', currentPhase === 'Expedição' || currentPhase === 'Completed' ? 'text-purpledark' : 'text-gray2')}>Expedição</p>
+                    <div className='w-30 h-30 flex flex-col justify-end items-center'>
+                        <img className='w-[125px] h-[125px]' onClick={() => changePhase('Expedição')} src={currentPhase === 'Expedição' ? expedicaoConcluido : currentPhase === 'Completed' ? concluido : expedicao} />
+                        <p className={clsx('text-center font-semibold text-[1rem]', currentPhase === 'Expedição' || currentPhase === 'Completed' ? 'text-purpledark' : 'text-gray2 opacity-66')}>Expedição</p>
                     </div>
                 </div>
-                <div className='mt-10'>
-                    <progress className='w-full h-1 border-0 rounded-full 
+                <div className='mt-10 flex items-center justify-center'>
+                    <progress className='w-[80%] h-1.5 border-0 rounded-full 
          [&::-webkit-progress-bar]:rounded-full [&::-webkit-progress-bar]:bg-gray 
          [&::-webkit-progress-value]:rounded-full [&::-webkit-progress-value]:bg-purpledark from-purple to-purpledark 
-         [&::-moz-progress-bar]:bg-purpledark' value={50} max={100}></progress>
+         [&::-moz-progress-bar]:bg-purpledark' value={25} max={100}></progress>
                 </div>
             </div>
-            <div>
-                <p>Olá, Manassés!</p>
-            </div>
-            <div>
-                <div>
-                    <div></div>
-                    <div></div>
+            <div className='w-full flex flex-col items-center'>
+                <div className='bg-[#FEF5FF] flex items-center p-5 mt-10 w-[80%] h-14 font-medium  rounded-2xl'>
+                    <p className='pl-5 text-purpledark font-medium text-[20px]'>
+                        Olá, Manassés! Estamos preparando o seu pedido.
+                    </p>
                 </div>
-                <div>
-                    <div></div>
+            </div>
+            <div className='w-full flex items-center justify-center mt-18'>
+                <div className='flex flex-col text-xl font-semibold w-[40%] text-start'>
+                    <div className='flex text-gray2 opacity-66 gap-3'>
+                        Tempo de produção:
+                        <h1 className='text-purpledark'>
+                            00:00:26
+                        </h1>
+                    </div>
+                    <div className='flex text-gray2 opacity-66 gap-3'>
+                        Ordem de produção:
+                        <h1 className='text-purpledark'>
+                            5588
+                        </h1>
+                    </div>
+                </div>
+                <div className='flex w-[40%] items-center justify-end'>
+                    <div className='text-purpledark text-2xl font-semibold border-2 border-purpledark rounded-xl pt-2 pb-2 pl-2 pr-2'>
+                        FECHAR
+                    </div>
                 </div>
             </div>
         </div>
