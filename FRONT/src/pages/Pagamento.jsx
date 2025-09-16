2a6014770c2f4df4b183e66867adf0d04d3e8d86
BRENDA MAGANELI CASIMIRO <brenda_casimiro@estudante.sesisenai.org.br>
2025-09-16 17:10:03 -0300

    tela pagamentos

--- FRONT/src/pages/Pagamento.jsx
+++ FRONT/src/pages/Pagamento.jsx
@@ -1,6 +1,6 @@
 //imagens
 import Logo from "../assets/logo.svg";
-
+import foto1 from "../assets/SKYNSNature1.svg";
 //componentes
 import CarrosselPQ from "../components/CarrosselPQ";
 
@@ -8,19 +8,19 @@ function Pagamento() {
   return (
     <div>
       <CarrosselPQ />
-      <div class="w-400 border-b border-b-blackwhite/20 flex items-center justify-center p-4">
+      <div class="w-[100%] border-b border-b-blackwhite/20 flex items-center justify-center p-4">
         <img class=" w-30" src={Logo} alt="" />
       </div>
 
       <div class="flex">
         {/* lado esquerdo */}
-        <div class="w-[60%] p-5">
+        <div class="w-[60%] p-5 mr-[40%]">
           {/* Entrega */}
           <div class="flex flex-col p-10 justify-center pl-[40%]">
             <p class="text-2xl font-secondary text-blackwhite/80 ">Entrega</p>
             <label class="mt-3 text-blackwhite/80 font-semibold">CEP</label>
             <input
-              class="text-sm p-1 mt-1 border-2 px-2 border-purple w-85 rounded-[5px]"
+              class="text-sm p-2 mt-1 border-1 px-2 border-blackwhite/60 w-85 rounded-[5px] focus:border-purpledark"
               type="text"
               name=""
               id=""
@@ -32,7 +32,7 @@ function Pagamento() {
               Cupom de desconto
             </label>
             <input
-              class="text-sm p-1 mt-3 border-2 px-2 border-purple w-85 rounded-[5px]"
+              class="text-sm p-2 mt-3 border-1 px-2 border-blackwhite/60 w-85 rounded-[5px] focus:border-purpledark"
               type="text"
               name=""
               id=""
@@ -62,7 +62,7 @@ function Pagamento() {
               Número do Cartão
             </label>
             <input
-              class="text-sm p-1 mt-1 border-2 px-2 border-purple w-85 rounded-[5px]"
+              class="text-sm p-2 mt-1 border-1 px-2 border-blackwhite/60 w-85 rounded-[5px] focus:border-purpledark"
               type="text"
               name=""
               id=""
@@ -75,7 +75,7 @@ function Pagamento() {
                 Data de expiração
               </label>
               <input
-                className="text-sm p-1 mt-1 border-2 px-2 border-purple w-40 rounded-[5px]"
+                className="text-sm p-2 mt-1 border-1 px-2 border-blackwhite/60 w-40 rounded-[5px] focus:border-purpledark"
                 type="text"
                 placeholder="ex: 10/30"
               />
@@ -85,34 +85,97 @@ function Pagamento() {
                 Código de segurança
               </label>
               <input
-                className="text-sm p-1 mt-1 border-2 px-2 border-purple w-40 rounded-[5px]"
+                className="text-sm p-2 mt-1 border-1 px-2 border-blackwhite/60 w-40 rounded-[5px] focus:border-purpledark"
                 type="text"
                 placeholder="ex: 975"
               />
             </div>
           </div>
-           <div class="flex flex-col p-6 justify-center pl-[40%]">
+          <div class="flex flex-col p-6 justify-center pl-[40%]">
             <label class="mt-3 text-blackwhite/80 font-semibold">
               Titular do Cartão
             </label>
             <input
-              class="text-sm p-1 mt-1 border-2 px-2 border-purple w-85 rounded-[5px]"
+              class="text-sm p-2 mt-1 border-1 px-2 border-blackwhite/60 w-85 rounded-[5px] focus:border-purpledark"
               type="text"
               name=""
               id=""
               placeholder="ex: Manassés Marcelino"
             />
           </div>
+
+          <div class="flex gap-2 justify-center pl-[10%]">
+            <input type="checkbox" name="" id="" />
+            <label htmlFor=""> Salvar forma de pagamento 30 dias</label>
+          </div>
+
+          <div class="flex flex-col p-10 justify-center pl-[40%] ">
+            <div class="flex justify-between w-79 items-center pb-10">
+              <label class="mt-3 text-blackwhite/90 font-bold text-[22px]">
+                Valor Total
+              </label>{" "}
+              <p class="mt-3 font-medium text-2xl text-purpledark"> R$290,90</p>
+            </div>
+
+            <div>
+              <button class="bg-purpledark w-85 p-2 rounded-[5px] cursor-pointer text-white text-[17px] font-medium">
+                Pagar
+              </button>
+            </div>
+          </div>
         </div>
 
         {/* lado Direito */}
-        <div class="w-[40%] bg-blackwhite/10">
-          {/* Entrega */}
-          <div class="flex flex-col p-10">
-            <p>Entrega</p>
-            <label>CEP</label>
-            <input type="text" name="" id="" />
+        <div className="absolute right-0 top-27 w-[40%] h-[150vh] bg-blackwhite/10 overflow-y-auto">
+
+        <div class="fixed right-0 top-27 w-[40%] h-[90%] overflow-y-auto">
+          {/* pedidos */}
+          <div class="flex flex-col p-10 gap-5">
+            <p class="text-[20px]">Resumo de Pedidos</p>
+            <div class="flex  gap-4 w-98 ">
+              <img class="w-23" src={foto1} alt="" />
+              <div class="flex flex-col ">
+                <p class="text-blackwhite/80">
+                  Sérum Rejuvenescedor Nocturne 45+ - 300g pele madura
+                </p>
+                <p class="mt-3 text-[19px] text-purpledark font-semibold">
+                  R$89,90
+                </p>
+              </div>
+            </div>
+            <div class="flex  gap-4 w-98 ">
+              <img class="w-23" src={foto1} alt="" />
+              <div class="flex flex-col ">
+                <p class="text-blackwhite/80">
+                  Sérum Rejuvenescedor Nocturne 45+ - 300g pele madura
+                </p>
+                <p class="mt-3 text-[19px] text-purpledark font-semibold">
+                  R$89,90
+                </p>
+              </div>
+            </div>
+            <div class="flex  gap-4 w-98 ">
+              <img class="w-23" src={foto1} alt="" />
+              <div class="flex flex-col ">
+                <p class="text-blackwhite/80">
+                  Sérum Rejuvenescedor Nocturne 45+ - 300g pele madura
+                </p>
+                <p class="mt-3 text-[19px] text-purpledark font-semibold">
+                  R$89,90
+                </p>
+              </div>
+            </div>
+
+            <div class="flex gap-5 mt-6">
+              <button class="cursor-pointer border font-semibold border-purpledark p-1 px-5 text-purpledark rounded-[5px]">
+                Limpar Pedido
+              </button>{" "}
+              <button class="bg-purpledark rounded-[5px] p-1 px-7 cursor-pointer text-white">
+                Escolher Mais Produtos
+              </button>
+            </div>
           </div>
+        </div>
         </div>
       </div>
     </div>
