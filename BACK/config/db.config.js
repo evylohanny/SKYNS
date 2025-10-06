ebc619af14c687eb1bf574c1ffefcf06e60315a5
VitorMatt <vitor.azevedoo2812@gmail.com>
2025-10-05 23:40:27 -0300

    Conexão Banco Hospedado

--- BACK/config/db.config.js
+++ BACK/config/db.config.js
@@ -1,15 +1,18 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "SKYNS",
-  password: "skyns",
+  user: "skyns_user",      
+  host: "dpg-d3eo583uibrs73ce2mpg-a.oregon-postgres.render.com",     
+  database: "skyns",
+  password: "cWh5XqYSL5NKxzosKIoYTNRNvquvpcLp",
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.connect()
-  .then(() => console.log("📦 Conectado ao PostgreSQL!"))
+  .then(() => console.log("📦 Conectado ao PostgreSQL Render!"))
   .catch(err => console.error("Erro ao conectar:", err));
 
 module.exports = pool;
