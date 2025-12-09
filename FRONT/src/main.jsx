218810c7f9a1af7eda885bf3abf09616cb3f2290
evylohanny <evelyn_l_silva@estudante.sesisenai.org.br>
2025-12-09 20:10:18 -0300

    ajuste em erro

--- FRONT/src/main.jsx
+++ FRONT/src/main.jsx
@@ -3,13 +3,9 @@ import { RouterProvider } from "react-router-dom";
 import router from "./routes/router.jsx";
 import { GlobalContextProvider } from "./context/GlobalContext.jsx";
 import "./index.css";
-import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById("root")).render(
   <GlobalContextProvider>
     <RouterProvider router={router} ></RouterProvider>
   </GlobalContextProvider>,
-  <GoogleOAuthProvider clientId="YOUR_GOOGLE_CLIENT_ID_HERE">
-    <main />
-  </GoogleOAuthProvider>
 );
